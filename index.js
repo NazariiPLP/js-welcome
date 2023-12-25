cfb942dc325b2bc27bd1c0d6509ca826cc75c6ed
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2023-12-25 20:21:47 +0200

    Continue Functions

--- index.js
+++ index.js
@@ -19,12 +19,34 @@ function ім'яФункції(аргумент1, аргумент 2) {
 function greetingUser(userName) {
   // userName - абстракція, на якій ми зав'язуємо логіку робот функції
   // userName - формальний параметр
-  console.log('Hello, ' + userName);
+  console.log("Hello, " + userName);
 }
 
 // Використання функції
 // Параметри, які передаються при виклику функції - фактичні параметри
-greetingUser('Alex'); // 'Alex' - агумент функції, фактичний параметр
-greetingUser('John');
-greetingUser('Sam');
-greetingUser('Solar');
+greetingUser("Alex"); // 'Alex' - агумент функції, фактичний параметр
+greetingUser("John");
+greetingUser("Sam");
+greetingUser("Solar");
+
+// Задача: Створіть функцію, яка приймає число, додає до нього 5 і виводить результат у консоль
+
+function addFive(number) {
+  let result = Number(number) + 5;
+  console.log(result);
+  return result;
+}
+
+const addFiveResult = addFive(5);
+// Задача: Написати фукнцію, яка складає два числа і повертає результат розрахунку
+
+function addTwoNumbers(num1, num2) {
+  // console.log("Hi from function");
+  const result = num1 + num2;
+  return result;
+}
+
+const addTwoNumbersResult = addTwoNumbers(2, 5);
+console.log(addTwoNumbersResult);
+
+console.log(addTwoNumbersResult + addFiveResult); // 10 + 7 = 17
