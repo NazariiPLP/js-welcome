5a79c897bf250f00cd15616c91a1aad0f600ccdc
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-04 20:49:13 +0200

    Constructor welcome

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-/* Задача
-Визначте у себе об'єкт кота.
-Опишіть його властивості і декілька методів - бігти, спати, нявкати
+/* Способи створення об'єкта
+
+const cat = { // літеральне створення об'єкта
+
+}
+
+const cat2 = Object(); // Функція-конструктор
+
+const cat3 = new Object(); // Функція-конструктор з оператором new
 */
 
-const cat = {
-  color: "Orange",
-  breed: "Stray",
-  age: "0,5",
-  eat: function () {
-    return "I am eating!";
-  },
-  run: function () {
-    return "I am running!";
-  },
-  sleep: function () {
-    return "I am sleeping";
-  },
-  meow: function () {
-    return "Meow!";
-  },
-};
+// Конструктор визначає шаблон або "скелет" об'єктів, які будуть створюватись
+
+/* Угоди у функціях конструкторах:
+
+1. Ім'я функції-конструктора має починатись з великої літери
+2. Функція-конструктор повинна виконуватись лише за допомогою операора new
+*/
+
+// Задача: написати функцію-конструктор для створення кота
+
+function Cat(name, color, breed, age) {
+  this.name = name; // this - посилання на новоствореного кота
+  this.color = color;
+  this.breed = breed;
+  this.age = age;
+  this.run = function () {
+    return this.name + " is running";
+  };
+}
+
+// Задача: створити декілька (двох) котів за допомогою фунції-конструктора
+
+const cat1 = new Cat("Bublik", "white", "stray", 3);
+const cat2 = new Cat("Murzik", "black", "siam", 2);
