d9329aaa066670703e718114bbb68577ed082de7
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-04 21:06:23 +0200

    Constructor practice task

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-/* Способи створення об'єкта
-
-const cat = { // літеральне створення об'єкта
-
-}
-
-const cat2 = Object(); // Функція-конструктор
-
-const cat3 = new Object(); // Функція-конструктор з оператором new
+/* Задача: 
+Написати функцію-конструктор для створення собаки (визначити ім'я, колір, вік).
+Створити декілька собак за допомогою фунції-конструктора.
 */
 
-// Конструктор визначає шаблон або "скелет" об'єктів, які будуть створюватись
-
-/* Угоди у функціях конструкторах:
-
-1. Ім'я функції-конструктора має починатись з великої літери
-2. Функція-конструктор повинна виконуватись лише за допомогою операора new
-*/
-
-// Задача: написати функцію-конструктор для створення кота
-
-function Cat(name, color, breed, age) {
-  this.name = name; // this - посилання на новоствореного кота
-  this.color = color;
-  this.breed = breed;
+function dog(name, color, age) {
+  this.name = name;
+  this.colot = color;
   this.age = age;
-  this.run = function () {
-    return this.name + " is running";
-  };
 }
 
-// Задача: створити декілька (двох) котів за допомогою фунції-конструктора
-
-const cat1 = new Cat("Bublik", "white", "stray", 3);
-const cat2 = new Cat("Murzik", "black", "siam", 2);
+const dog1 = new dog("Jack", "black", 3);
+const dog2 = new dog("Whesley", "white", 2);
+const dog3 = new dog("Russel", "white - orange", 4);
