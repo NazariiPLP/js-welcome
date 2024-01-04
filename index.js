00b10ccb0249974ab982086e4bb65fbfe17178c6
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-04 20:24:46 +0200

    Obj task

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
-/* Спроби створення об'єктів
-
-const cat = {
-  // літеральне створення об'єктів
-};
-
-const cat2 = Object(); // Функція-конструктор
-
-const cat3 = new Object(); // Функція-конструктор з оператором new
-
+/* Задача
+Визначте у себе об'єкт кота.
+Опишіть його властивості і декілька методів - бігти, спати, нявкати
 */
 
 const cat = {
-  name: "Murzik",
   color: "Orange",
   breed: "Stray",
-  age: 2,
+  age: "0,5",
   eat: function () {
-    return "I'm eating";
+    return "I am eating!";
+  },
+  run: function () {
+    return "I am running!";
   },
   sleep: function () {
-    return "I'm sleeping";
+    return "I am sleeping";
+  },
+  meow: function () {
+    return "Meow!";
   },
 };
-
-console.log(cat.age); // 2
-
-cat.age = cat.age + 1;
-
-console.log(cat.age); // 3
-
-cat.sleep = undefined; // Bad practice
-
-delete cat.sleep; // Good practice
-
-// Adding properties
-
-// Задача: додати ім'я друга Мурзика до об'єкту Мурзика
-
-cat.friend = "Tuzik";
-
-cat["favorite food"] = "fish";
-
-cat.meow = function() {
-  return 'Meow'
-}
