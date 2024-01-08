f903ed746a1cbfc90d4672efa466fa12290c8591
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-08 18:56:47 +0200

    Practice task

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ const valueFromUser = 19;
 
 console.log(greets[valueFromUser]); //Hello
 // greets[19]
+
+/*Задача: у нас є об'єкт, ключем в якому є день тижня, а значенням - плани на цей день
+Прийняти від користувача (через промпт) назву дня тижня і видати alert, що заплановано у користувача на цей день */
+
+const day = {
+  Mon: "drive lesson",
+  Tue: "dentist visit",
+  Wed: "go to party",
+  Thu: "hard work",
+  Fri: "chill",
+  Sat: "go to park",
+};
+
+const dayFromUser = prompt(
+  "Type the day of week to get ypur plan: \nMon \nTue \nWed \nThu \nFri \nSat"
+);
+alert(day[dayFromUser] ? obj[dayFromUser] : "Error happaned");
