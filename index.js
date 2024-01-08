e3bf475a800b3a8b91350635fbf286d4e8429141
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-08 19:09:20 +0200

    In operator example

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const day = {
-  Mon: "drive lesson",
-  Tue: "dentist visit",
-  Wed: "go to party",
-  Thu: "hard work",
-  Fri: "chill",
-  Sat: "go to park",
-  "favorite colot": "white",
-  0: "value",
+function greeting(user) {
+  return `Hello ${"name" in user ? user.name : "Anonym"}`;
+}
+
+const user = {
+  name: "John",
+  email: "john.doe@gmail.com",
 };
-
-// operator in
-// in - бінарний оператор
-// 'key' in obj
-
-console.log("Mon" in day);
-console.log("favorite colot" in day);
-console.log(0 in day);
-console.log("name" in day); //false
