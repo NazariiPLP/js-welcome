78876469be0667d926aaf72a0141971a3ef8e3de
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-08 19:02:29 +0200

    OPerator in

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// Задача: є об'єкт з різними версіями привітань для користувача
-// Вивести користувачу привітання, залежно від значення, яке він ввів.
-
-const greets = {
-  15: "Hi",
-  17: "Ya, whats up",
-  19: "Hello",
-  30: "Good morning",
-};
-
-const valueFromUser = 19;
-
-console.log(greets[valueFromUser]); //Hello
-// greets[19]
-
-/*Задача: у нас є об'єкт, ключем в якому є день тижня, а значенням - плани на цей день
-Прийняти від користувача (через промпт) назву дня тижня і видати alert, що заплановано у користувача на цей день */
-
 const day = {
   Mon: "drive lesson",
   Tue: "dentist visit",
@@ -23,9 +5,15 @@ const day = {
   Thu: "hard work",
   Fri: "chill",
   Sat: "go to park",
+  "favorite colot": "white",
+  0: "value",
 };
 
-const dayFromUser = prompt(
-  "Type the day of week to get ypur plan: \nMon \nTue \nWed \nThu \nFri \nSat"
-);
-alert(day[dayFromUser] ? obj[dayFromUser] : "Error happaned");
+// operator in
+// in - бінарний оператор
+// 'key' in obj
+
+console.log("Mon" in day);
+console.log("favorite colot" in day);
+console.log(0 in day);
+console.log("name" in day); //false
