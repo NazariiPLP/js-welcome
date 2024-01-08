1c997558f987f329bea25986cea26e6524b16958
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-08 19:21:32 +0200

    Loop: for-in

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-function greeting(user) {
-  return `Hello ${"name" in user ? user.name : "Anonym"}`;
-}
-
 const user = {
   name: "John",
-  email: "john.doe@gmail.com",
+  lastName: "Doe",
+  age: 20,
+  "favorite color": "red",
+  movie: "Blue moon",
+  avatar: "http://",
 };
+
+// for..in - використовується для перпебору (ітерації) ключів в об'єкті
+/*
+for (variable in object) {
+  Код, що виконується для кожної властивості
+}
+
+
+variable - змінна, яка буде приймати значення ключів властивостей
+object - об'єкт властивості (ключі) якого потрібно перпебрати
+
+*/
+
+// Задача: у об'єкті user вивести назви всіх ключів і властивості
+
+for (let key in user) {
+  console.log(`${key} : ${user[key]}`); // key ---> value
+}
