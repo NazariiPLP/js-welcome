2b0bdb6aa3013fdd9cb428f4bebc92cb1159f98f
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-08 20:46:06 +0200

    Practice task

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
-const user = {
-  name: "John",
-  lastName: "Doe",
-  age: 20,
-  "favorite color": "red",
-  movie: "Blue moon",
-  avatar: "http://",
-};
-
-// for..in - використовується для перпебору (ітерації) ключів в об'єкті
 /*
-for (variable in object) {
-  Код, що виконується для кожної властивості
-}
-
-
-variable - змінна, яка буде приймати значення ключів властивостей
-object - об'єкт властивості (ключі) якого потрібно перпебрати
-
+Задача: є об'єкт з зарплатами по всіх відділах.
+Написати функцію, яка повертає загальну суму, витрачену на зарплатню всіх відділів.
 */
 
-// Задача: у об'єкті user вивести назви всіх ключів і властивості
-
-for (let key in user) {
-  console.log(`${key} : ${user[key]}`); // key ---> value
-}
-
-/*
-Задача: написати функцію, яка приймає об'єкт і виводить всі значення всіх властивостей, оформлені *...*
-А ключі - "..."
-// "key1": *value1*
-*/
-
-const obj = {
-  key1: "value 1",
-  key2: "value 2",
-  key3: "value 3",
+const departmentSalary = {
+  HR: 120000,
+  development: 5500000,
+  PR: 50000,
+  marketing: 120000,
+  asisstant: undefined,
+  financials: null,
 };
 
-function objValue(x) {
-  for (let key in x) {
-    console.log(`"${key}":*${obj[key]}*`);
+function sumOfSalary(obj) {
+  sum = 0;
+  for (let key in obj) {
+    if (obj[key] !== undefined && obj[key] !== null) {
+      sum += obj[key];
+    }
   }
+  return sum;
 }
 
-objValue(obj);
+console.log(sumOfSalary(departmentSalary));
