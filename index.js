d17e44e2b1ff078cc5c70231ee2479a2d1de263e
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-09 18:13:11 +0200

    Practice task

--- index.js
+++ index.js
@@ -3,17 +3,28 @@
 Написати функцію, яка повертає загальну суму, витрачену на зарплатню всіх відділів.
 */
 
-const departmentSalary = {
+const departmentSalaryInCompany1 = {
   HR: 120000,
   development: 5500000,
   PR: 50000,
   marketing: 120000,
-  asisstant: undefined,
+  assistant: undefined,
   financials: null,
 };
 
+const departmentSalaryInCompany2 = {
+  HR: 500000,
+  JS: 5500000,
+  PR: 50000,
+  QA: 800000,
+  clojure: 250000,
+  frontend: 365800,
+  strategyExecution: undefined,
+  cLevel: null,
+};
+
 function sumOfSalary(obj) {
-  sum = 0;
+  let sum = 0;
   for (let key in obj) {
     if (obj[key] !== undefined && obj[key] !== null) {
       sum += obj[key];
@@ -22,4 +33,5 @@ function sumOfSalary(obj) {
   return sum;
 }
 
-console.log(sumOfSalary(departmentSalary));
+console.log(sumOfSalary(departmentSalaryInCompany1));
+console.log(sumOfSalary(departmentSalaryInCompany2));
