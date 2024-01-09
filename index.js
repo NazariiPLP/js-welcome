771bffb6714edf9a211b50d9f878a0daba44134a
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-09 19:09:34 +0200

    Array practice task 1

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-// Є певний user
-
-const userTelephone1 = "380957382856";
-const userTelephone2 = "380678092562";
-const userTelephone3 = "380993648236";
-
-const userTelephones = {
-  0: "380957382856",
-  1: "380678092562",
-  2: "380993648236",
-};
-
-// Масив (Array)
-
-// Оголошення масиву
-
-// 1 спосіб
-const arr = [2, 4, 6, 7, 150, 789, 123];
-
-// 2 спосіб
-const arr2 = new Array(2, 4, 6, 7);
-
-// Способи доступу до елементів масиву
-arr[0]; // Зчитування значень елементів масиву
-arr[0] = 200; // Присвоєння нових значень для елементів масиву
-
-// Задача: вивесит всі значення масиву в консолі
-
-/* варіант1 
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
+/* Задача 1
+Дано масив [3, 6, 8, 2, 3, 5, 1].
+Напишіть функцію, яка приймає масив в якості аргументу і повертає суму всіх елементів масиву
 */
 
-// варіант2
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Задача: у нас є масив чисел
-const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20];
-
-for (let i = 0; i < numbersArray.length; i++) {
-  if (numbersArray[i] % 2 === 0) {
-    console.log(numbersArray[i]);
+function arraySum(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
+  return sum;
 }
+
+console.log(arraySum(array1));
+
+/*
+
+*/
