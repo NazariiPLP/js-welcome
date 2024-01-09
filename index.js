b371662ed791dbc1c1ee33d83f534a54a7416ad9
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-09 19:28:47 +0200

    Array practice task 2

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-/* Задача 1
+/* Задача 2
 Дано масив [3, 6, 8, 2, 3, 5, 1].
-Напишіть функцію, яка приймає масив в якості аргументу і повертає суму всіх елементів масиву
+Напишіть функцію, яка знаходить найбільше значення серед елементів масиву.
 */
 
-const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array1 = [3, 6, 8, 2, 3, 5, 1];
 
-function arraySum(array) {
-  let sum = 0;
+function biggestNum(array) {
+  let maxNum = 0;
   for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+    if (array[i] > maxNum) {
+      maxNum = array[i];
+    }
   }
-  return sum;
+  return maxNum;
 }
 
-console.log(arraySum(array1));
-
-/*
-
-*/
+console.log(biggestNum(array1));
