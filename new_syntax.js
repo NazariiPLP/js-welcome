ffc514b173ba170b9d8611b2331c05ee12524c28
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-23 20:32:36 +0200

    Class practice task

--- new_syntax.js
+++ new_syntax.js
@@ -31,7 +31,6 @@ const user1 = new User("Alex", "Dane", 20);
 const user2 = new User("Dari", "Dane", 23);
 
 /*
-
 Правила класів
 
 1. constructor() при створені класу - обов'язковий
@@ -39,3 +38,26 @@ const user2 = new User("Dari", "Dane", 23);
 3. Конструктор має бути тільки один 
 4. Як і у функіях-конструкторах назва класу записується з великої літери
 */
+
+/* Задача
+Написати клас Worker.
+У працівника є ім'я, прізвище, ставка за робочий день і кількість відпрацьованих днів у цьому місяці
+Метод, який повертає зарплатню цього робітника за поточний місяць
+*/
+
+class Worker {
+    constructor (name, lastName, rate, daysWorked) {
+        this.name = name;
+        this.lastName = lastName;
+        this.rate = rate;
+        this.daysWorked = daysWorked;
+    }
+
+    getSalary() {
+        return this.rate * this.daysWorked;
+    }
+}
+
+const worker1 = new Worker ('Alex', 'Fisherman', 200, 20);
+
+console.log(worker1.getSalary());
