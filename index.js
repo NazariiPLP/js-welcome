ebe53ce39679d81de00002614a5d847dbcee4e93
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-23 13:38:01 +0200

    Recursive function practice task

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function pow(x, n) {
   let result = 1; // змінна у якій ми будемо накопичувати добуток
 
   // множимо result на x , n разів у циклі
-  for(let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     result *= x;
   }
 
@@ -22,12 +22,14 @@ console.log(pow(2, 3)); // 8
 // Рекурсивне мислення: спрощуємо собі завдання і викликаємо функцією саму себе
 
 function powRecursive(x, n) {
-  if(n === 1) {
+  if (n === 1) {
     return x;
   } else {
     return x * powRecursive(x, n - 1);
   }
 }
+
+console.log(powRecursive(2, 3)); // 8
 
 /*            if n === 1 -->> x
             /
@@ -57,12 +59,36 @@ powRecursive(x,n) =
 
 // Кількість вкладених викликів - глибина рекурсії
 
-// Ми стремимся до того, що глибина рекурсії не має перевищувати 10000 викликів
+// Ми прагнемо до того, що глибина рекурсії не має перевищувати 10000 викликів
 /* Задача 1
 
 Знайти факторіал числа за допомогою рекурсії
 
 */
+number4 = 4;
+number5 = 5;
+
+function factorial(n) {
+  let result = 1;
+  for (i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(number4));
+console.log(factorial(number5));
+
+function factorialRecursive(n) {
+  if (n === 1) {
+    return n;
+  } else {
+    return n * factorialRecursive(n - 1);
+  }
+}
+
+console.log(factorialRecursive(number4));
+console.log(factorialRecursive(number5));
 
 /* Задача 2
 
