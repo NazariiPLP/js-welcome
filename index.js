2d78750085a8d3cc95ff7d6004060b249396d578
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-30 20:07:22 +0200

    MyArray in class

--- index.js
+++ index.js
@@ -67,5 +67,54 @@ const dog = AnimalFactory.createAnimal("dog", "Tuzik");
 dog.makeSound(); // Гав-гав
 
 // Створити об'єкт кота
-const cat = AnimalFactory.createAnimal('cat', 'Murzik');
+const cat = AnimalFactory.createAnimal("cat", "Murzik");
 cat.makeSound(); // Мяу-мяу
+
+// Переписати myAraay на класи
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+  }
+  
+  push() {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
+    return this.length;
+  }
+  
+  pop() {
+    if (this.length > 0) {
+      // 1. Зберегти останній елемент
+      const lastItem = this[this.length - 1];
+      // 2. Видалити останній елемент з масиву
+      delete this[this.length - 1];
+      // 3. Зменшити довжину масиву на 1
+      this.length--;
+      // 4. Повернути останній елемент
+      return lastItem;
+    } else {
+      return undefined;
+    }
+  }
+  
+  
+  forEach(callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  }
+}
+
+const arr = new MyArray();
+arr.push(1);
+arr.push(2);
+arr.push(3, 2, 5);
+
+// Відконсольлогувати квадрати кожного числа в масиві arr
+
+arr.forEach((item) => {
+  console.log(item ** 2);
+});
