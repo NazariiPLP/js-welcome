ce5af3e77dff813580042bf3882218d1118c06eb
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-01-30 20:48:13 +0200

    Add map mathod to MyArray

---
+++ MyArray.js
@@ -0,0 +1,44 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+  }
+
+  push() {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
+    return this.length;
+  }
+
+  pop() {
+    if (this.length > 0) {
+      // 1. Зберегти останній елемент
+      const lastItem = this[this.length - 1];
+      // 2. Видалити останній елемент з масиву
+      delete this[this.length - 1];
+      // 3. Зменшити довжину масиву на 1
+      this.length--;
+      // 4. Повернути останній елемент
+      return lastItem;
+    } else {
+      return undefined;
+    }
+  }
+
+  forEach(callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  }
+
+  map(callback) {
+    // Створення пустого масиву, який є результуючим масивом
+    const newArray = new MyArray();
+
+
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,22 +1,60 @@
-/*
-Symbol - представляє собою унікальний ідентифікаотр.
-Кожен створений символ має свій власний унікальний ідентифікатор, який ніде і ніколи не буде повторюватись
+class MyArray {
+  constructor() {
+    this.length = 0;
+  }
 
-Особливість символів  - вони завжди унікальні
-Кожен створений символ, матиме унікальний ідентифікатор, який ніде і ніколи не буде повторюватись
+  push() {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
+    return this.length;
+  }
 
-Гарантія унікальності - єдине, для чого Symbol існує
+  pop() {
+    if (this.length > 0) {
+      // 1. Зберегти останній елемент
+      const lastItem = this[this.length - 1];
+      // 2. Видалити останній елемент з масиву
+      delete this[this.length - 1];
+      // 3. Зменшити довжину масиву на 1
+      this.length--;
+      // 4. Повернути останній елемент
+      return lastItem;
+    } else {
+      return undefined;
+    }
+  }
 
-*/
+  forEach(callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  }
 
-const mySymbol = Symbol(); // без new!!
+  map(callback) {
+    // 1. Просто створили пустий масив - він буде нашим результуючим масивом
+    const resultArray = new MyArray();
 
-// const symb2 = Symbol('My second symbol'); // label for human
+    // 2. Пройтись по поточному масиву від початку і до кінця
+    for(let i = 0; i < this.length; i++) {
+      // Запихуємо у результуючий масив значення, яке нам буде повертати (return) колбек
+      resultArray.push(callback(this[i], i, this));
+    }
 
-const obj = {
-  //  У об'єктах ключами можуть бути рядки або символи
-  test: 1,
-  [mySymbol]: 123,
-};
+    // 3. Повернути як результат роботи методу map - результуючий масив
+    return resultArray;
+  }
+}
 
-console.log(obj[mySymbol]); // 123
+const arr = new MyArray();
+
+arr.push(3, 2, 1, 2, 3);
+
+// Задача: створити новий масив, який буде містити квадрати чисел з масиву arr
+
+const doubleNumbers = arr.map((item) => {
+  return item ** 2;
+});
+
+// const doubleNumbers = arr.map(item => item ** 2);
