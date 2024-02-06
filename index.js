f977e882b68c8637ca3d119d47fc39b242fe69e6
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-06 20:53:55 +0200

    Closure

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
-const monitor = {
-  sizes: {
-    height: {
-      value: 30,
-      scale: "cm // 1",
-    },
-    width: {
-      value: 50,
-      scale: "cm // 2",
-    },
-  },
-  brightness: 750,
-  refresh: {
-    value: 144,
-    scale: "Ggrc",
-  },
-  color: "black",
-  resolution: "4k",
-};
+// Замикання довзоляє функціям зберігати доступ до змінних, навіть коли функції завершили своє виконання
+// Замикання - здатність функції запам'ятовувати локальну область видимості
+
+let value = 10; // глобальна область видимості
 
 
-/*
-Існує 3 види деструктуризації у JS:
-+ 1. Деструктиризація об'єктів
-2. Деструктиризація вхідних параметрів
-+ 3. Деструктиризація масивів
-*/
- 
-const arr = [1, 2, 3, 4, 5, 6];
-// const firstElement = arr[0];
-
-const [firstElement, seconfElement, ...restOfArray] = arr;
-
-function getFullName({firstName, lastName, ...restObjects}) { // все інше, окрім firstName, lastName - ігнорується
-    console.log(restObjects);
-    return`${firstName} ${lastName}`;
+function wrapper() {
+    let value = 20; // локальна область видимості
+    
+    console.log("WRAPPER function", value);
+    
+    return function log() {
+      console.log("LOG function", value);
+    }
 }
 
-const user2 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 42,
-    geolocation: '42.24345312 56.11214545',
-    browser: 'Chrome'
+//////
+
+// function counter() {
+//     let i = 0;
+//     i++;
+//     return i;
+// }
+
+//////
+
+function makeCounter() {
+    let i = 0;
+    return function() {
+        return i++; // i - змінна у замиканні
+    }
 }
 
-console.log(getFullName(user2));
+const fn = makeCounter();
+console.log(fn()); // 0
+console.log(fn()); // 1
+console.log(fn()); // 2
+console.log(fn()); // 3
+
+
+//////
+
+function makeCounterVersion2() {
+    let counter = 0;
+
+    return {
+        increment() {
+            return ++counter;
+        },
+        decrement() {
+            return --counter;
+        }
+    }
+}
+
+const fnObj = makeCounterVersion2();
